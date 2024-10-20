b963f41a71e5d9cf29585350bb1b7f1d70450688
nksealex <no-reply@example.com>
2024-10-20 13:35:09 +0200

    build on standalone mode

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -2,6 +2,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: 'standalone',
 	images: {
 		unoptimized: true,
 	},

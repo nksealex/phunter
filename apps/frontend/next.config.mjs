a2a16cb512f780b743a851f1f3b16355a044bdc3
nksealex <no-reply@example.com>
2024-10-20 13:44:48 +0200

    revert standalone

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -2,7 +2,6 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'standalone',
 	images: {
 		unoptimized: true,
 	},
